5f8fb86ec1f84d82423598f3ac6b76b2a77689b5
Fanghao Chen <chen.fanghao517@gmail.com>
2014-05-15 15:03:54 -0700

    initial commit

---
+++ MrSelfie/MSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MSAppDelegate.h
+//  MrSelfie
+//
+//  Created by Fanghao Chen on 5/15/14.
+//  Copyright (c) 2014 MSStorm8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MrSelfie/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MrSelfie
+//
+//  Created by Fanghao Chen on 5/15/14.
+//  Copyright (c) 2014 MSStorm8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MSAppDelegate class]));
+    }
+}

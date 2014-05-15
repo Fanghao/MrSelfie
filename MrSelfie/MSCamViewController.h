3eca5d180859821b96187ce6c0f504647d9aaf85
Fanghao Chen <chen.fanghao517@gmail.com>
2014-05-15 15:29:13 -0700

    added storyboard

---
+++ MrSelfie/MSCamViewController.h
@@ -0,0 +1,13 @@
+//
+//  MSCamViewController.h
+//  MrSelfie
+//
+//  Created by Fanghao Chen on 5/15/14.
+//  Copyright (c) 2014 MSStorm8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSCamViewController : UIViewController
+
+@end

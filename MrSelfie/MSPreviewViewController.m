047376ecf72a694ac2253099381bee20696aee75
Weixi Yen <weixiyen@gmail.com>
2014-05-15 17:36:12 -0700

    saves to gif now

--- MrSelfie/MSPreviewViewController.m
+++ MrSelfie/MSPreviewViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "MSPreviewViewController.h"
+#import <ImageIO/ImageIO.h>
+#import <MobileCoreServices/MobileCoreServices.h>
+
 
 @interface MSPreviewViewController ()
 
@@ -27,9 +30,27 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+//    
+//    self.photos = @[
+//                    [UIImage imageNamed:@"IMG_2443.jpg"],
+//                    [UIImage imageNamed:@"IMG_2444.jpg"],
+//                    [UIImage imageNamed:@"IMG_2445.jpg"],
+//                    [UIImage imageNamed:@"IMG_2446.jpg"],
+//                    [UIImage imageNamed:@"IMG_2447.jpg"],
+//                    [UIImage imageNamed:@"IMG_2448.jpg"],
+//                    [UIImage imageNamed:@"IMG_2449.jpg"],
+//                    [UIImage imageNamed:@"IMG_2450.jpg"],
+//                    [UIImage imageNamed:@"IMG_2451.jpg"],
+//                    [UIImage imageNamed:@"IMG_2452.jpg"],
+//                    [UIImage imageNamed:@"IMG_2453.jpg"],
+//                    [UIImage imageNamed:@"IMG_2454.jpg"],
+//                    [UIImage imageNamed:@"IMG_2455.jpg"],
+//                    ];
 
     self.currentIndex = 0;
     [self showNextImage];
+    
+    [self createAnimatedGif];
 }
 
 - (void)showNextImage {
@@ -44,17 +65,67 @@
     
     // default case, increment and show the next image
     self.currentIndex += 1;
-    [self performSelector:@selector(showNextImage) withObject:nil afterDelay:0.250];
+    [self performSelector:@selector(showNextImage) withObject:nil afterDelay:0.2];
 }
 
 - (IBAction)share:(id)sender {
+    NSLog(@"share...");
     
+    // open up fb share
 }
 
 - (IBAction)retake:(id)sender {
     [self dismissViewControllerAnimated:NO completion:^(void){
         
     }];
+}
+
+- (void)createAnimatedGif {
+    int frameCount = self.photos.count;
+    
+    NSDictionary *fileProperties = @{
+                                 (__bridge id)kCGImagePropertyGIFDictionary: @{
+                                         (__bridge id)kCGImagePropertyGIFLoopCount: @0, // 0 means loop forever
+                                         }
+                                 };
+    
+    NSDictionary *frameProperties = @{
+                                      (__bridge id)kCGImagePropertyGIFDictionary: @{
+                                              (__bridge id)kCGImagePropertyGIFDelayTime: @0.2f, // a float (not double!) in seconds, rounded to centiseconds in the GIF data
+                                              }
+                                      };
+    
+    NSDictionary *finalFrameProperties = @{
+                                      (__bridge id)kCGImagePropertyGIFDictionary: @{
+                                              (__bridge id)kCGImagePropertyGIFDelayTime: @3.0f, // a float (not double!) in seconds, rounded to centiseconds in the GIF data
+                                              }
+                                      };
+    
+    NSURL *documentsDirectoryURL = [[NSFileManager defaultManager] URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:YES error:nil];
+    NSURL *fileURL = [documentsDirectoryURL URLByAppendingPathComponent:@"animated.gif"];
+    
+    CGImageDestinationRef destination = CGImageDestinationCreateWithURL((__bridge CFURLRef)fileURL, kUTTypeGIF, frameCount, NULL);
+    CGImageDestinationSetProperties(destination, (__bridge CFDictionaryRef)fileProperties);
+    
+    for (NSUInteger i = 0; i < frameCount; i++) {
+        @autoreleasepool {
+            UIImage *image = [self.photos objectAtIndex:i];
+            
+            if (i == self.photos.count - 1) {
+                CGImageDestinationAddImage(destination, image.CGImage, (__bridge CFDictionaryRef)finalFrameProperties);
+            } else {
+                CGImageDestinationAddImage(destination, image.CGImage, (__bridge CFDictionaryRef)frameProperties);
+            }
+        }
+    }
+    
+    if (!CGImageDestinationFinalize(destination)) {
+        NSLog(@"failed to finalize image destination");
+    }
+    
+    CFRelease(destination);
+    
+    NSLog(@"url=%@", fileURL);
 }
 
 @end

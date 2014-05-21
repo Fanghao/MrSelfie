167948f09fd27aa20192391508443bc050928417
Weixi Yen <weixiyen@gmail.com>
2014-05-21 00:22:43 -0700

    magic numbers

--- MrSelfie/MSPreviewViewController.m
+++ MrSelfie/MSPreviewViewController.m
@@ -53,9 +53,9 @@ static NSString *const GIF_FILE_NAME = @"animated.gif";
 
     CGFloat alpha = 0.0f;
 
-    for (int i=0; i<30; i++) {
+    for (int i=0; i<40; i++) {
 
-        if (i > 25 && i < 30) {
+        if (i > 35 && i < 40) {
             UIImage *test = [self addFlashOverlay:[self fixOrientationOfImage:self.firstImage] withAlpha:alpha];
 
             alpha += .2;
@@ -219,7 +219,7 @@ static NSString *const GIF_FILE_NAME = @"animated.gif";
 }
 
 - (void)showNextImage {
-    if (self.currentIndex > 25 && self.currentIndex < 30) {
+    if (self.currentIndex > 35 && self.currentIndex < 40) {
         UIImage *image = [self.photos objectAtIndex:self.currentIndex];
         UIImage* flippedImage = [UIImage imageWithCGImage:image.CGImage
                                                     scale:image.scale orientation:UIImageOrientationUpMirrored];
